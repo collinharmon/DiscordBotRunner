46e571ad2000d18bad4e7a9100d1509f90aaec47
collinharmon <collin.harmon1@gmail.com>
2021-07-16 22:59:03 -0700

    Modified main.py to obtain .ini file for Discord Tokens via command line. If multiple then create multiple instances.

--- main.py
+++ main.py
@@ -2,14 +2,28 @@ import concurrent.futures.thread
 from discord_bot_runner.classes.discord_bot_runner import DiscordBotRunner
 import discord
 import configparser
+import sys
 
 def main():
   config = configparser.ConfigParser()
-  config.read("./discord_bot_runner/keys/tokens.ini")
-  TOKEN = config['tokens']['discord']
-  intents = discord.Intents.all()
-  client = DiscordBotRunner(intents=intents)
-  client.run(TOKEN)
+  config_files = [arg for arg in sys.argv if arg.endswith('.ini')]
+  if len(config_files) < 1:
+    print("Error: No .ini file for the Discord token(s) were provided as an argument.")
+    return
+
+  clients = []
+  for cf in config_files:
+    config.read(cf)
+    try:
+      token = config['tokens']['discord']
+    except Exception as e:
+      print("KeyError: Invalid .ini file provided.")
+      continue
+    else:
+      intents = discord.Intents.all()
+      client = DiscordBotRunner(intents=intents)
+      client.run()
+      clients.append(client)
   
 if __name__ == "__main__":
   main()
